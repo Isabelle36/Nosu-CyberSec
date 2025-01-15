e0eaa2b90f2f6c9e4afc2e81853389e44056319b
jzf21 <jozefneelamkavil2003@gmail.com>
2025-01-15 13:41:42 +0530

    fix signup bug

--- frontend-cyber/src/Components/LoginPage/SignUp.js
+++ frontend-cyber/src/Components/LoginPage/SignUp.js
@@ -16,11 +16,14 @@ export default function SignUp() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:8000/auth/signup", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER_URL}/auth/login`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        }
+      );
       const data = await response.json();
       if (response.ok) {
         alert("Sign-Up successful!");
